14b230511c6659ceb233523737a1d65aeeeafeb9
teraclitos <tefans12@gmail.com>
2023-06-12 17:17:13 -0300

    update mongoose conection

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require(`express`);
+const app = express();
+const morgan = require(`morgan`);
+const cors = require(`cors`);
+const path = require(`path`);
+
+if (process.env.NODE_ENV !== `production`) {
+  require("dotenv").config();
+}
+require("./dataBase");
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan(`dev`));
+app.use(cors());
+
+// app.use("/", express.static(path.join(__dirname, "tmp")));
+
+// const routes = require("./routes");
+
+// app.use("/", routes);
+
+app.listen(PORT, () => {
+  console.log("back ejecutandose en el puerto: ", PORT);
+  console.log("enviroment:", process.env.NODE_ENV);
+});
