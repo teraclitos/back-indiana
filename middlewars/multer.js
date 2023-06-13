4b46148bce14ce11849decc20b67565d39fa0160
teraclitos <tefans12@gmail.com>
2023-06-13 18:30:08 -0300

    multer added

---
+++ middlewars/multer.js
@@ -0,0 +1,36 @@
+const multer = require(`multer`);
+const path = require("path");
+const { v4: uuidv4 } = require("uuid");
+let folder = "";
+
+if (process.env.NODE_ENV === `production`) {
+  folder = `../../../tmp`;
+} else {
+  folder = `../tmp`;
+}
+
+module.exports = uploadFile = () => {
+  const storage = multer.diskStorage({
+    fileFilter: (req, file, cb) => {
+      const filetypes = `/jpeg|png|jpg/`;
+      const mimetypes = filetypes.test(mimetypes.toLowerCase());
+      const extname = filetypes.test(
+        path.extname(file.originalname).toLowerCase()
+      );
+
+      if (mimetypes && extname) {
+        return cb(null, true);
+      } else {
+        ("error: incorrect format of the image ");
+      }
+    },
+    destination: path.join(__dirname, folder),
+    filename: (req, file, cb) => {
+      cb(null, uuidv4() + path.extname(file.originalname));
+    },
+  });
+
+  const upload = multer({ storage }).array(`images`);
+
+  return upload;
+};
