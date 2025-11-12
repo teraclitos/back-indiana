32884ed9142daedcc0697c97de91ff2d4c7fa96d
Francisco <tefans12@gmail.com>
2025-11-12 15:13:57 -0300

    restore multer

--- middlewars/multer.js
+++ middlewars/multer.js
@@ -1,33 +1,59 @@
 const multer = require('multer')
+const path = require('path')
+const { v4: uuidv4 } = require('uuid')
 
-const fileFilter = (req, file, cb) => {
-  const allowed = /image\/(jpeg|jpg|png|webp)/
-  allowed.test(file.mimetype) ? cb(null, true) : cb(new Error('Incorrect format of the image'))
+let folder = ''
+if (process.env.NODE_ENV === 'production') {
+  folder = '../../../tmp'
+} else {
+  folder = '../tmp'
 }
 
-const limits = { files: 30, fileSize: 10 * 1024 * 1024 } // 10MB c/u
+const uploadFile = () => {
+  const storage = multer.diskStorage({
+    destination: path.join(__dirname, folder),
+    filename: (req, file, cb) => {
+      cb(null, uuidv4() + path.extname(file.originalname))
+    }
+  })
 
-const upload = multer({
-  storage: multer.memoryStorage(),
-  fileFilter,
-  limits
-})
+  const fileFilter = (req, file, cb) => {
+    const allowedExtensions = ['.jpeg', '.jpg', '.png', '.webp']
+    const extname = path.extname(file.originalname).toLowerCase()
+    if (allowedExtensions.includes(extname)) {
+      cb(null, true)
+    } else {
+      cb(new Error('Incorrect format of the image'))
+    }
+  }
 
-const uploadOneImage = upload.single('image')
-const uploadExtraPhotos = upload.array('extraPhotos')
-const uploadCarPhotos = upload.fields([
-  { name: 'fotoPrincipal', maxCount: 1 },
-  { name: 'fotoHover', maxCount: 1 },
-  { name: 'fotosExtra' } // múltiples
-])
+  const limits = {
+    files: 30 // ahora se permiten 30 archivos
+  }
+
+  const uploadOneImage = multer({ storage, fileFilter }).single('image')
+
+  const uploadExtraPhotos = multer({ storage, fileFilter, limits }).array('extraPhotos')
+
+  const uploadCarPhotos = multer({ storage, fileFilter, limits }).fields([
+    { name: 'fotoPrincipal', maxCount: 1 },
+    { name: 'fotoHover', maxCount: 1 },
+    { name: 'fotosExtra' }
+  ])
+
+  return { uploadExtraPhotos, uploadOneImage, uploadCarPhotos }
+}
 
 const errFormatImages = 'Incorrect format of the image'
+
 const handleMulterErrors = (err, req, res, next) => {
   if (err instanceof multer.MulterError || err.message === errFormatImages) {
     return res.status(400).json({ error: true, msg: err.message })
+  } else if (err) {
+    return res.status(500).json({ error: true, msg: err.message })
   }
-  if (err) return res.status(500).json({ error: true, msg: err.message })
+
   next()
 }
 
-module.exports = { uploadOneImage, uploadExtraPhotos, uploadCarPhotos, handleMulterErrors }
+module.exports = { uploadFile, handleMulterErrors }
